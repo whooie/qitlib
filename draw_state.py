0d69f74ed3294c3f04b795f49db9eba97b305a26
whooie <wchooie@gmail.com>
2021-06-13 12:51:15 -0500

    interactive state-drawing programs

--- draw_state.py
+++ draw_state.py
@@ -1,5 +1,3 @@
 import lib.dirac as dirac
 
-dirac.draw_bloch_interactive(
-    [r"$\left|0\right\rangle$", r"$\left|1\right\rangle$"]
-)
+dirac.draw_bloch_interactive(["0", "1"])
---
+++ draw_state_measure.py
@@ -0,0 +1,3 @@
+import lib.dirac as dirac
+
+dirac.draw_bloch_interactive_measure(["0", "1"])
